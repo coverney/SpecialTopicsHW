9180095ea879ec971c57da6d0b5ddfadea8164fb
coverney <cassandra.overney@students.olin.edu>
2020-04-13 15:39:52 -0700

    Implemented IP solver for CVRP

--- VRP/solver.py
+++ VRP/solver.py
@@ -4,8 +4,10 @@ using an integer programming and then a local search approach. This code has
 been adapted from functions written by Alice Paul.'''
 
 import picos as pic
+from picos import RealVariable, Constant, BinaryVariable
 import numpy as np
 from read_files import read_file_type_A, read_file_type_C
+from scipy.special import comb
 
 # Integer programming approach
 def cvrp_ip(C,q,K,Q,obj=True):
@@ -22,16 +24,53 @@ def cvrp_ip(C,q,K,Q,obj=True):
         objective_value: value of the minimum travel cost
         x: matrix representing number of routes that use each arc
     '''
-    # TODO: add in destination node (same distances as source & demand = 0)
-
+    # Add in destination node
+    # It should have the same distances as source and its demand equals 0
+    q2 = np.append(q, 0)
+    col = C[:,0]
+    C2 = np.hstack((C, np.atleast_2d(col).T))
+    row = C2[0,:]
+    C3 = np.vstack ((C2, row) )
     # set up the picos problem
     prob = pic.Problem()
-
-    # TODO: add variables, constraints, and objective function!
-
-    x = []
-    objective_value = 0
-
+    K_value = K
+    Q_value = Q
+    # define variables
+    N = Constant("N", range(len(q2))) # clients {1,2,...N} plus destination, column vector
+    K = Constant("K", K_value) # number of vehicles
+    Q = Constant("Q", Q_value) # vehicle capacity
+    q = Constant("q", q2) # demand for each client
+    c = Constant("c", C3, C3.shape) # travel cost from i to j
+    x = BinaryVariable("x", C3.shape) # whether edge is in the tour
+    u = RealVariable("u", len(q2)) # cumulative quantity of good delivered between origin and client i
+    prob.set_objective("min", pic.sum([c[i,j]*x[i,j] for j in range(C3.shape[1]) for i in range(C3.shape[0])])) # double check this
+    # add constraints
+    # all u values should be >= q and <= Q
+    for i in range(len(q2)):
+        prob.add_constraint(u[i] >= q[i])
+        prob.add_constraint(u[i] <= Q)
+    # for all edges, u_i-u_j+Q*x <= Q - qj
+    for i in range(C3.shape[0]) :
+        for j in range(C3.shape[1]):
+            prob.add_constraint(u[i] - u[j] + Q * x[i,j] <= Q - q[j])
+    # make sure that only 1 vehicle leaves every client
+    # make sure rows of x sum to 1
+    for n in N[1:-1]:
+        prob.add_constraint(pic.sum(x[n,:])==1)
+    # make sure that only 1 vehicle enters every client
+    # make sure cols of x sum to 1
+    for n in N[1:-1]:
+        prob.add_constraint(pic.sum(x[:,n])==1)
+    # make sure that no more than K vehicles leave the origin
+    prob.add_constraint(pic.sum(x[0,:]) <= K)
+    # make sure that no more than K vehicles enter the origin
+    prob.add_constraint(pic.sum(x[:,-1]) <= K)
+    # make sure that the number of vehicles that leave the origin should
+    # equal the number of vehicles coming back in
+    prob.add_constraint(pic.sum(x[0,:]) == pic.sum(x[:,-1]))
+    # solve integer program
+    res = prob.solve(solver='cplex')
+    objective_value = pic.sum([c[i,j]*x[i,j] for j in range(C3.shape[1]) for i in range(C3.shape[0])])
     return objective_value, x
 
 # Local search approach (OPTIONAL)
